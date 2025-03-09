76a8992b5b11e0015958950bea80b06e500f471b
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-03-10 02:19:53 +0530

    about-updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import { motion } from "framer-motion";
 import tree from "../assets/growing-tree.png";
 
 const About = () => {
   return (
-    <div className="max-w-screen w-full min-h-screen flex flex-col items-center mt-20 justify-center">
-      <h1 className=" text-4xl md:text-7xl text-center text-white py-40 md:p-40 w-full uppercase bg-gradient-to-r from-red-500 to-orange-600 border-2 border-white">
+    <div className="max-w-screen w-full min-h-screen flex flex-col items-center mt-20 justify-center ">
+      <motion.h1
+        initial={{ opacity: 0.4, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className=" text-4xl md:text-7xl text-center text-white py-40 md:p-40 w-full mt-6 md:mt-0 uppercase bg-gradient-to-r from-red-500 to-orange-600 border-2 border-white"
+      >
         About Us
-      </h1>
-      <h2 className="relative custom-border custom-border-2 w-96 h-15 text-xl md:text-5xl text-center text-white my-5">
+      </motion.h1>
+      <motion.h2
+        initial={{ opacity: 0.4, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="relative custom-border custom-border-2 w-96 h-15 text-xl md:text-5xl text-center text-white my-5"
+      >
         Introduction
-      </h2>
+      </motion.h2>
       <div className="flex flex-col-reverse md:flex-row-reverse items-center max-w-screen-xl mx-auto">
-        <p className="text-sm md:text-xl text-center text-white max-w-screen-md">
+        <motion.p
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true, amount: 0.4 }}
+          transition={{ duration: 0.5 }}
+          className="text-sm md:text-xl text-center text-white max-w-screen-md"
+        >
           Anicomic Services, a branch of Anicomic India, was founded in 2024
           with the vision to provide comprehensive creative solutions for
           businesses and individuals looking to enhance their visual and digital
@@ -24,13 +40,28 @@ const About = () => {
           production but has since grown into a full-fledged creative agency
           offering a range of services, including music production, audio
           editing, brand strategy, and more.
-        </p>
-        <img className="h-[400px]" src={tree} alt="" />
+        </motion.p>
+        <motion.img 
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true, amount: 0.4 }}
+        transition={{ duration: 0.5 }}
+        className="h-[400px]" src={tree} alt="" />
       </div>
-      <h2 className="relative custom-border custom-border-2 w-96 h-15 text-xl md:text-5xl text-center text-white my-5">
+      <motion.h2 
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.4 }}
+      transition={{ duration: 0.5 }}
+      className="relative custom-border custom-border-2 w-80 h-15 text-xl md:text-5xl text-center text-white my-5">
         Vision
-      </h2>
-      <p className="text-sm md:text-xl text-center text-white max-w-screen-md mb-10">
+      </motion.h2>
+      <motion.p 
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      viewport={{ once: true, amount: 0.6 }}
+      transition={{ duration: 0.9 }}
+      className="text-sm md:text-xl text-center text-white max-w-screen-md mb-10">
         At Anicomic Services, we believe that every brand has a unique story,
         and our mission is to help bring that story to life through compelling
         visuals, engaging videos, and immersive soundscapes. We aim to be a
@@ -38,7 +69,7 @@ const About = () => {
         on quality, innovation, and customer satisfaction. Our vision is to
         empower businesses and creators by delivering professional, affordable,
         and personalised creative solution that is resonate with their audience
-      </p>
+      </motion.p>
     </div>
   );
 };
